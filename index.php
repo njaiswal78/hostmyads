61dca2b5839daeb80f21d734a4c5e4b0dd347b78
Natrajan Jaiswal <njaiswal78@gmail.com>
2019-02-18 23:24:44 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quickly launch Newspaper, TV, Radio, Cinema, LED &amp; 3D digital Ads</title>
+</head>
+<body>
+<?php
+echo "Select location, category, day, medium and submit your ad now";
+?>
+</body>
+</html>

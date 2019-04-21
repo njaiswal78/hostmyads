38ce5072740e7478fd039e716c118447af794980
Natrajan Jaiswal <njaiswal78@gmail.com>
2019-04-22 02:11:41 +0530

    f

--- index.php
+++ index.php
@@ -3,10 +3,42 @@
 <head>
 	<title>Quickly launch Newspaper, TV, Radio, Cinema, LED &amp; 3D digital Ads</title>
 </head>
+<style type="text/css">
+	body{
+		width: 100%;
+		height: 100%;
+	}
+	#top-header{
+		margin: 0px;
+		color: black;
+		font-weight: bold;
+		font-size: 35px;
+		height: 10vh;
+		border: 1px solid black;
+}
+	#content{
+		background-image: url("back.jpeg");
+		background-repeat: no-repeat;
+		width: 100%;
+		height: 90vh;
+		border: 1px solid blue;
+		background-size: 100% 50vh;
+	}
+</style>
+<script type="text/javascript"></script>
 <body>
+<div id="top-header">
+	<p style="margin-left: 10%;float: left;">Host<span style="color:red;">My</span>Ads<span style="font-size: 10px;line-height: 80%"> Serving Fastest Newspaper Ads</span></p>
+<div style="width: 20%;border: 1px solid blue;float: right;text-align: center;height: 10vh;background-color: red;color: white;font-weight: bold;font-size: 25px;">
+	<p>+91 9305812900</p>
+</div>
+</div>
+<div id="content">
 <?php
 echo "<h1>Select location, category, day, medium and submit your ad now</h1>";
 echo "<h2>Advertise options: Newspaper, TV, Radio, LED Screens, Holograms etc.</h2>";
 ?>
+
+</div>
 </body>
 </html>
